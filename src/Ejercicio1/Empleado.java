bae42eba1080bd7e34a87138fb5a8a157ff2666f
Valentina Conde <68123729+valentinaconde@users.noreply.github.com>
2025-03-27 12:51:28 -0300

    first commit

---
+++ src/Ejercicio1/Empleado.java
@@ -0,0 +1,29 @@
+package Ejercicio1;
+
+public class Empleado {
+	private int id;
+	private String nombre;
+	private int edad;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+}
